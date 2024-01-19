e59d14a7116cadd989e72d516a2836876aa4f31f
WilliamDelnington <107051066+WilliamDelnington@users.noreply.github.com>
2024-01-19 22:05:33 +0700

    Update StudentMark.py
    The deleted methods are already exist in StudentMarkManagement.

--- 2.student.mark.oop/StudentMark.py
+++ 2.student.mark.oop/StudentMark.py
@@ -1,5 +1,4 @@
 from Student import Student
-from Course import Course
 
 class StudentMark(Student):
     def __init__(self, studentId=None, studentName=None, birthdate=None):
@@ -12,31 +11,3 @@ class StudentMark(Student):
 
     def getMarks(self):
         return self.__marks
-
-    def coursePicking(self):
-        courseId = input("Write course id: ")
-        while True:
-            for c in Course.getAllCourses():
-                if c.getCourseId().lower() == courseId.lower():
-                    return courseId
-            print("Course not found.")
-            courseId = input("Write course id: ")
-
-    def setSpecificMark(self):
-        Course.printAllCourses()
-        courseId = self.coursePicking()
-        for s in Student.getAllStudents():
-            s.setMark(courseId)
-
-    def printCourseMark(self):
-        Course.printAllCourses()
-        courseId = self.coursePicking()
-        print("Student Marks: ")
-        print("Student ID----Student Name----Course Id----Mark")
-        for s in Student.getAllStudents():
-            print("{}----{}----{}----{}".format(
-                s.getStudentId(),
-                s.getStudentName(),
-                courseId,
-                s.getMarks[courseId.upper()]
-            ))
